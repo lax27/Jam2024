b7270f87c879d2dd1cb98eb55c65853d4747e738
Voidlax27 <alex.taravilla@enti.cat>
2024-01-19 13:07:24 +0100

    CameraFollowRetoques

--- Assets/Scripts/CameraFolow.cs
+++ Assets/Scripts/CameraFolow.cs
@@ -6,10 +6,12 @@ public class CameraFolow : MonoBehaviour
 {
     [SerializeField] private GameObject target;
     [SerializeField] private float speed;
+    [SerializeField] private float Yoffset;
+    [SerializeField] private float Zoffset;
 
     private void Update()
     {
-        Vector3 newPos = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
+        Vector3 newPos = new Vector3(target.transform.position.x, target.transform.position.y + Yoffset ,Zoffset);
         transform.position = Vector3.Slerp(transform.position, newPos, speed);
     }
 
